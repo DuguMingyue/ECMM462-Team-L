66a5307370945268a9f1fbcecf5ef0a59eb2631b
mrAdamTweedie <61285048+AdamTweedie@users.noreply.github.com>
2022-12-30 12:02:12 +0000

    validMove()

--- Project2/src/world.java
+++ Project2/src/world.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
 
 public class world{
     int timestamp = 0;// Time in this world
@@ -9,7 +12,7 @@ public class world{
     double performance = 0; // Evaluation
     crane crane = new crane(); // In hot storage there is only 1 crane
     craneSchedule schedule = new craneSchedule();
-    craneMove currentMove = new craneMove();
+    craneMove currentMove = new craneMove(crane);
 
     arriveStack aristk = new arriveStack();
     bufferStack bufstk = new bufferStack(bufferamount);
@@ -29,11 +32,49 @@ public class world{
         }
     }
 
-    //
     public static boolean validMove(world world){
-        boolean rst = true;
+        craneMove currentMove = world.currentMove;
+        if (currentMove.sourceId == 0) {
+            if (currentMove.blockId != world.aristk.stack.get(0).id) {
+                return false; // block not found
+            }
+            if (currentMove.targetId == 2 && currentMove.blockId == world.aristk.stack.get(0).id && !world.aristk.stack.get(0).ready) {
+                return false; // block is not ready
+            }
+        }
+        if (currentMove.sourceId == 1) {
+            int[] ids = new int[world.bufstk.bufferamount];
+            block[] blocksOnTop = new block[world.bufstk.bufferamount];
+            for (ArrayList<block> stack : world.bufstk.stack) {
+                ids[world.bufstk.stack.indexOf(stack)] = stack.get(stack.size()-1).id;
+                blocksOnTop[world.bufstk.stack.indexOf(stack)] = stack.get(stack.size()-1);
+            }
+            if (IntStream.of(ids).noneMatch(x -> x == currentMove.blockId)) { // if block not top in any buffer
+                return false; // block not found
+            }
 
-        return rst;
+            for (block ontopblock : blocksOnTop) {
+                if (currentMove.targetId == 2 && ontopblock.id == currentMove.blockId && !ontopblock.ready) {
+                    return false; // block is not ready
+                }
+            }
+        }
+        if (currentMove.sourceId == 2) {
+            return false; // invalid source
+        }
+        if (currentMove.targetId == 0) {
+            return false; // invalid target
+        }
+        if (currentMove.targetId == 1) {
+            int stacksTotalCapacity = 0;
+            for ( ArrayList<block> stack : world.bufstk.stack) {
+                stacksTotalCapacity += stack.size();
+            }
+            if (stacksTotalCapacity >= world.bufstk.totalCapacity) {
+                return false; // height violation
+            }
+        }
+
+        return true; // otherwise return true
     }
-
 }
